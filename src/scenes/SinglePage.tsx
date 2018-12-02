22ff2fdfdca98202f057e0390c80b8a743802abe
makiri <kireew.martin@icloud.com>
2018-12-02 20:35:50 +0100

    removed comments

--- src/scenes/SinglePage.tsx
+++ src/scenes/SinglePage.tsx
@@ -201,8 +201,7 @@ const Introduction = styled.div`
 
   border: 3px solid #70798c;
   border-radius: 4px;
-  /* border-bottom: 3px solid #70798c; */
-  /* border-color: #0e79b2; */
+
   transition: background 1s ease-in-out, box-shadow 1s ease-in-out;
   :hover {
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
