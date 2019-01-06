e626fe5e9c7190ef7dc9895922bacb8641d2ecff
makiri <kireew.martin@icloud.com>
2019-01-06 21:30:27 +0100

    new form try

--- src/components/HireButton/HirePopUp.tsx
+++ src/components/HireButton/HirePopUp.tsx
@@ -55,7 +55,6 @@ export default class HirePopUp extends Component<Props, State> {
         data-netlify-honeypot='bot-field'
       >
         <input type='hidden' name='form-name' value='contact' />
-
         <p>
           <label>
             Your Name: <input type='text' name='name' />
