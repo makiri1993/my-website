ab137ea730ea8969696d8f6cc957a086301a03ed
makiri <kireew.martin@icloud.com>
2019-01-06 21:51:13 +0100

    new try with forms

--- src/components/HireButton/HirePopUp.tsx
+++ src/components/HireButton/HirePopUp.tsx
@@ -1,4 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component, ChangeEvent } from 'react'
+
+const encode = (data: any) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
 
 interface Props {}
 
@@ -9,65 +15,70 @@ interface State {
 }
 
 export default class HirePopUp extends Component<Props, State> {
-  constructor(props: Props) {
+  constructor(props: any) {
     super(props)
     this.state = { name: '', email: '', message: '' }
   }
 
   /* Here’s the juicy bit for posting the form submission */
 
-  // private handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-  //   console.log('bin submit')
-  //   fetch('/', {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  //     body: this.encode({ 'form-name': 'contact', ...this.state }),
-  //   })
-  //     .then(() => alert('Success!'))
-  //     .catch(error => alert(error))
+  handleSubmit = (e: any) => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(() => alert('Success!'))
+      .catch(error => alert(error))
 
-  //   e.preventDefault()
-  // }
+    e.preventDefault()
+  }
 
-  // private handleChange = (e: any) => {
-  //   // console.table(e.target)
-  //   console.log(e.target)
-  //   console.log(e.target.name)
-  //   console.log(e.target.value)
-  //   this.setState({ [e.target.name]: e.target.value } as any)
-  // }
+  handleChange = () => (
+    e: ChangeEvent<HTMLInputElement>,
+    value: string
+  ): void => {
+    const key = e.currentTarget.name
 
-  // private encode = (data: any) => {
-  //   return data
-  //     .map(
-  //       (key: any) =>
-  //         encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
-  //     )
-  //     .join('&')
-  // }
+    if (Object.keys(this.state).includes(key)) {
+      this.setState({ [key]: value } as Pick<State, keyof State>)
+    }
+  }
 
   render() {
+    const { name, email, message } = this.state
     return (
-      <form
-        name='contact'
-        method='post'
-        data-netlify='true'
-        data-netlify-honeypot='bot-field'
-      >
-        <input type='hidden' name='form-name' value='contact' />
+      <form onSubmit={this.handleSubmit}>
         <p>
           <label>
-            Your Name: <input type='text' name='name' />
+            Your Name:{' '}
+            <input
+              type='text'
+              name='name'
+              value={name}
+              onChange={this.handleChange}
+            />
           </label>
         </p>
         <p>
           <label>
-            Your Email: <input type='email' name='email' />
+            Your Email:{' '}
+            <input
+              type='email'
+              name='email'
+              value={email}
+              onChange={this.handleChange}
+            />
           </label>
         </p>
         <p>
           <label>
-            Message: <textarea name='message' />
+            Message:{' '}
+            <textarea
+              name='message'
+              value={message}
+              onChange={this.handleChange}
+            />
           </label>
         </p>
         <p>
