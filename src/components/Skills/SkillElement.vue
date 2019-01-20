698201cae2cc8e10b6b7b0defee77a81285830c0
makiri <kireew.martin@icloud.com>
2019-01-20 21:04:06 +0100

    added skill comp

---
+++ src/components/Skills/SkillElement.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="skill-element mx-3 py-4 px-5" :style="skill.level">{{skill.name}}</div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+import { SkillsData } from '@/components/Skills/Skills.vue'
+
+@Component
+export default class SkillElement extends Vue {
+  @Prop() private skill!: SkillsData
+}
+</script>
+<style scoped>
+.skill-element {
+  display: block;
+  border-radius: var(--standard-border-radius);
+  border: var(--standard-border);
+}
+</style>
---
+++ src/components/Skills/Skills.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="skill-container">
+    <SkillElement v-for="(el,index) in skills" :key="index" :skill="el"/>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import SkillElement from '@/components/Skills/SkillElement.vue'
+import { Prop } from 'vue-property-decorator'
+
+export enum SKillLevel {
+  BEGINNER = '',
+  INTERMEDIATE = '',
+  HOBBY = '',
+}
+
+export interface SkillsData {
+  name: string
+  level: SKillLevel
+}
+
+@Component({
+  components: {
+    SkillElement,
+  },
+})
+export default class Skills extends Vue {
+  @Prop() private skills!: SkillsData[]
+}
+</script>
+<style scoped>
+.skill-container {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+}
+</style>
