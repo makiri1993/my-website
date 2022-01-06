eecd7b54a0f3c7deab8a447af541aeb0b16bea4f
makiri1993 <kireew.martin@icloud.com>
2022-01-06 10:18:08 +0100

    update deploy script

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 rustup target add wasm32-unknown-unknown
+chmod +x tailwindcss.sh
 cargo install --locked trunk
 trunk config show
 trunk build 
